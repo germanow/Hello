fa10af0bbbab26823bc70e6942e06f3723331cdc
Ivan <iwan.germanow@gmail.com>
2013-11-27 21:30:57 +0200

    Create level6.rb

---
+++ level6.rb
@@ -0,0 +1,42 @@
+class Player
+  def play_turn(warrior)
+  if (warrior.health<13)
+  then
+    if (warrior.feel(:backward).captive?)
+     then 
+      warrior.rescue!:backward
+     else
+      if (warrior.feel(:backward).wall?)
+      then warrior.rest!
+      else
+      warrior.walk!:backward
+      end
+    end
+  else
+     
+   if (warrior.feel.empty?)
+   then
+    if (warrior.health<20)
+     then
+      if(@health<=warrior.health)
+       then
+        warrior.rest!
+      else
+       warrior.walk!
+       end
+     else
+       warrior.walk!
+    end
+   else
+    if (warrior.feel.captive?)
+     then
+      warrior.rescue!
+     else
+      warrior.attack!
+    end
+   end
+   
+   end
+   @health=warrior.health
+  end
+end
